e2f0c5bd0feb36086ddbe7ed0d378eb7fc149604
Oopsybance <mishak784@uandex.ru>
2023-06-21 22:21:23 +0300

    9sprint

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# api_final
-api final
+# Yatube API
+
+API для социальной сети Yatube, где пользователи могут создавать посты, оставлять комментарии и подписываться на других пользователей.
+
+## Используемые технологии
+Python 3.9
+Django
+Django REST Framework
+pytest
+pytest-django
+python-decouple
+
+## Установка
+
+1. Клонируйте репозиторий по ссылке:
+https://github.com/Oopsybance/api_final_yatube
+
+2. Установите виртуальное окружение командой:
+```bash
+python -m venv venv
+```
+
+3. Активируйте виртуальное окружение:
+- На Windows:
+```bash
+source venv/Scripts/activate
+```
+- На Unix или MacOS:
+```bash
+source venv/bin/activate
+```
+
+4. Установите зависимости:
+```bash
+pip install -r requirements.txt
+```
+
+5. Создайте файл .env в корневом каталоге и установите ваши переменные окружения:
+```bash
+SECRET_KEY=your-secret-key
+```
+
+6. Выполните миграции:
+```bash
+python manage.py migrate
+```
+
+7. Создайте суперпользователя:
+```bash
+python manage.py createsuperuser
+```
+
+8. Запускаем проект:
+```bash
+python manage.py runserver
+```
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,85 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.serializers import CurrentUserDefault
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
-
-    class Meta:
-        fields = '__all__'
-        model = Post
-
-
-class CommentSerializer(serializers.ModelSerializer):
+    '''Сериализатор постов. '''
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        slug_field='username',
+        read_only=True,
     )
 
     class Meta:
-        fields = '__all__'
+        model = Post
+        fields = (
+            'id', 'text', 'author', 'image', 'group', 'pub_date', 'comments'
+        )
+        read_only_fields = ('comments',)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    '''Сериализатор комментариев. '''
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
         model = Comment
+        fields = ('id', 'text', 'author', 'created', 'post')
+        read_only_fields = ('post',)
+
+    def validate(self, data) -> dict:
+        if not data['text']:
+            raise serializers.ValidationError(
+                'Комментарий не может быть пустым'
+            )
+        return data
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    '''Сериализатор групп. '''
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    '''Сериализатор подписок. '''
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=False,
+        queryset=User.objects.all(),
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('id', 'user', 'following')
+        validators = (
+            serializers.UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message='Вы уже подписаны на данного автора!',
+            ),
+        )
+
+    def validate(self, data) -> dict:
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError(
+                'Вы не можете подписаться сам на себя!'
+            )
+        return data
